7ae0e78c2aa134ab2e9da726562a66c239eaccf7
曾令燊 <dvsseed@hotmail.com>
2020-02-14 15:35:46 +0800

    Update dbsettings.php

--- lib/dbsettings.php
+++ lib/dbsettings.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html;charset=UTF-8');
 define('MYSQL_ADDRESS', '127.0.0.1');
 define('MYSQL_USERNAME', 'root');
-define('MYSQL_PASSWORD', 'p@$$w0rd');
+define('MYSQL_PASSWORD', 'password@root');
 // define('MYSQL_USERNAME', 'druguser');
 // define('MYSQL_PASSWORD', '0HfpaWyU50v3');
 define('DATABASE', 'drugstorehouse');
